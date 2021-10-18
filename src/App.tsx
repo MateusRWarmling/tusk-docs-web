11c0ea80b8393de6fb4ea39c91552cb26b554401
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-18 14:02:07 -0300

    Add: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './Assets/styles/theme'
-import './App.css'
-import { Home } from './pages/Home'
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./Assets/styles/theme";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <ChakraProvider>
-      <Home />
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About}></Route>
+        </Switch>
+      </Router>
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
