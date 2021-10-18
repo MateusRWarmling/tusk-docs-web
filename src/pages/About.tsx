68efa1f9c01c9aa62435cdfcef6c162052ca133a
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-18 14:04:02 -0300

    Add: about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { Header } from "../components/Header";
+
+export function About() {
+  return (
+    <Flex>
+      <Header />
+    </Flex>
+  );
+}
