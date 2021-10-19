63a029f8f50b6e3a6d7c957a3c5347821a4125cd
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-19 12:04:15 -0300

    Add: components to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import { Box } from "@chakra-ui/react";
 import { Header } from "../components/Header";
 import { HomeFirstSection } from "../components/HomeFirstSection";
+import { HomeSecondSection } from "../components/HomeSecondSection";
+import { HomeThirdSection } from "../components/HomeThirdSection";
+import { SubscribeToNewsletter } from "../components/SubscribeToNewsletter";
 
 export function Home() {
   return (
     <Box>
       <Header />
       <HomeFirstSection />
+      <HomeSecondSection />
+      <HomeThirdSection />
+      <SubscribeToNewsletter />
     </Box>
   );
 }
