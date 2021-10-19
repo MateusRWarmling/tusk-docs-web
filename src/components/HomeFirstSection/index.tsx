59ca694770e2a360913a7991da5ef68a0d350314
Mateus Warmling <mateuswarmling@gmail.com>
2021-10-19 12:02:16 -0300

    Change: Home First Section

--- src/components/HomeFirstSection/index.tsx
+++ src/components/HomeFirstSection/index.tsx
@@ -1,14 +1,10 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
+import { SectionInfo } from "./SectionInfo";
 
 export function HomeFirstSection() {
   return (
-    <Flex w="1120px" marginX="auto" justifyContent="space-between">
-      <Flex fontFamily="ubuntu">
-        <Text fontWeight="bold" fontSize="56">
-          Deixa que o<br /> elefantinho da Tusk
-          <br /> lembra por você!!
-        </Text>
-      </Flex>
+    <Flex w="1200px" marginX="auto" justifyContent="space-between" mb="6rem">
+      <SectionInfo />
       <Image src="src\Assets\images\notebookImage.png" />
     </Flex>
   );
