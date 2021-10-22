9e940b412a5e52fc04c0822774787896cf4a3b08
MateusRWarmling <mateuswarmling@gmail.com>
2021-10-22 01:24:45 -0300

    refactor: create Register Button component

--- src/components/Header/RegisterButton.tsx
+++ src/components/Header/RegisterButton.tsx
@@ -1,21 +1,24 @@
 import { Box } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export function RegisterButton() {
   return (
-    <Box
-      as="button"
-      backgroundColor="blue.850"
-      color="white"
-      fontWeight="bold"
-      paddingY="12px"
-      paddingX="2.1rem"
-      borderRadius="6px"
-      transition="0.2s"
-      _hover={{
-        filter: "brightness(0.7)",
-      }}
-    >
-      Cadastrar
-    </Box>
+    <Link to="/register">
+      <Box
+        as="a"
+        backgroundColor="blue.850"
+        color="white"
+        fontWeight="bold"
+        paddingY="12px"
+        paddingX="2.1rem"
+        borderRadius="6px"
+        transition="0.2s"
+        _hover={{
+          filter: "brightness(0.7)",
+        }}
+      >
+        Cadastrar
+      </Box>
+    </Link>
   );
 }
--- src/components/HomeFirstSection/SectionInfo.tsx
+++ src/components/HomeFirstSection/SectionInfo.tsx
@@ -1,4 +1,5 @@
-import { Box, Text, Button, Flex } from "@chakra-ui/react";
+import { Box, Text, Flex } from "@chakra-ui/react";
+import { RegisterButton } from "../RegisterButton";
 
 export function SectionInfo() {
   return (
@@ -15,18 +16,7 @@ export function SectionInfo() {
         seus clientes? Agora você não precisa mais se <br /> preocupar que a
         Tusk cuida disso pra você!
       </Text>
-      <Button
-        backgroundColor="red.450"
-        transition="0.2s"
-        paddingY="35px"
-        paddingX="35px"
-        fontSize="18px"
-        color="white"
-        width="fit-content"
-        _hover={{ backgroundColor: "red.500" }}
-      >
-        Cadastre-se Agora!🎉
-      </Button>
+      <RegisterButton />
     </Flex>
   );
 }
