0c55ca401f7e38114c393672061d6c1f7cb6a488
MateusRWarmling <mateuswarmling@gmail.com>
2021-10-29 22:17:43 -0300

    Fix: Home style

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { theme } from "./Assets/styles/theme";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Login } from "./pages/Login";
+import { Dashboard } from "./pages/Dashboard";
+import { Clients } from "./pages/Clients";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About}></Route>
           <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/dashboard" component={Dashboard}></Route>
+          <Route exact path="/Clients" component={Clients}></Route>
         </Switch>
       </Router>
     </ChakraProvider>
