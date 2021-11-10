898f2e7a46be931c96ac2c3a3cf0bfd6935b76cd
MateusRWarmling <mateuswarmling@gmail.com>
2021-11-10 16:46:44 -0300

    Fix: view image modal

--- src/pages/Documents/DocumentsTable.tsx
+++ src/pages/Documents/DocumentsTable.tsx
@@ -44,6 +44,7 @@ export function DocumentsTable() {
   const { data: clientData } = useClients();
   const [imageUrl, setImageUrl] = useState("");
   const [openedModal, setOpenedModal] = useState(false);
+  const [openedImage, setOpenedImage] = useState("");
   const [localImageUrl, setLocalImageUrl] = useState<any>();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [completedCrop, setCompletedCrop] = useState<any>(null);
@@ -121,210 +122,220 @@ export function DocumentsTable() {
   };
 
   return (
-    <Flex w="100%" my="6" mx="auto">
-      <Box flex="1">
-        <Flex mb="8" justify="space-between" align="center">
-          <Button
-            type="submit"
-            size="lg"
-            fontSize="sm"
-            backgroundColor="blue.850"
-            color="white"
-            _hover={{ filter: "brightness(0.6)" }}
-            transition="0.2s"
-            mt="24px"
-            onClick={() => onOpen()}
-          >
-            Novo boleto
-          </Button>
-
-          <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="xl">
-            <DrawerOverlay />
-            <Box
-              as="form"
-              onSubmit={handleSubmit(handleCreateClient)}
-              encType="multipart/form-data"
+    <>
+      <Flex w="100%" my="6" mx="auto">
+        <Box flex="1">
+          <Flex mb="8" justify="space-between" align="center">
+            <Button
+              type="submit"
+              size="lg"
+              fontSize="sm"
+              backgroundColor="blue.850"
+              color="white"
+              _hover={{ filter: "brightness(0.6)" }}
+              transition="0.2s"
+              mt="24px"
+              onClick={() => onOpen()}
             >
-              <DrawerContent>
-                <DrawerHeader borderBottomWidth="1px">
-                  Cadastrar novo boleto
-                </DrawerHeader>
+              Novo boleto
+            </Button>
 
-                <DrawerBody>
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    gridGap="18px"
-                    w="100%"
-                  >
-                    <Stack gridGap="18px" w="100%">
-                      <FormControl id="title">
-                        <FormLabel
-                          fontSize="0.75rem"
-                          fontWeight="bold"
-                          color="#9FA2B4"
-                        >
-                          NOVO BOLETO
-                        </FormLabel>
+            <Drawer
+              isOpen={isOpen}
+              placement="right"
+              onClose={onClose}
+              size="xl"
+            >
+              <DrawerOverlay />
+              <Box
+                as="form"
+                onSubmit={handleSubmit(handleCreateClient)}
+                encType="multipart/form-data"
+              >
+                <DrawerContent>
+                  <DrawerHeader borderBottomWidth="1px">
+                    Cadastrar novo boleto
+                  </DrawerHeader>
 
-                        <InputGroup>
-                          <Input
-                            type="text"
-                            {...register("title")}
-                            placeholder="Titulo do boleto"
-                            isRequired
-                          />
-                        </InputGroup>
-                      </FormControl>
-
-                      <FormControl id="description">
-                        <FormLabel
-                          fontSize="0.75rem"
-                          fontWeight="bold"
-                          color="#9FA2B4"
-                        >
-                          Descrição
-                        </FormLabel>
-
-                        <InputGroup>
-                          <Input
-                            type="text"
-                            {...register("description")}
-                            placeholder="Mais informações"
-                            isRequired
-                          />
-                        </InputGroup>
-                      </FormControl>
-
-                      <FormControl id="clientId">
-                        <FormLabel
-                          fontSize="0.75rem"
-                          fontWeight="bold"
-                          color="#9FA2B4"
-                        >
-                          CLIENTE
-                        </FormLabel>
-
-                        <InputGroup marginBottom="24px">
-                          <Select
-                            isRequired
-                            {...register("clientId")}
-                            placeholder="Selecione o cliente"
+                  <DrawerBody>
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      gridGap="18px"
+                      w="100%"
+                    >
+                      <Stack gridGap="18px" w="100%">
+                        <FormControl id="title">
+                          <FormLabel
+                            fontSize="0.75rem"
+                            fontWeight="bold"
+                            color="#9FA2B4"
                           >
-                            {clientData?.clients.map((client) => (
-                              <option key={client.id} value={client.id}>
-                                {client.name}
-                              </option>
-                            ))}
-                          </Select>
-                        </InputGroup>
+                            NOVO BOLETO
+                          </FormLabel>
 
-                        <FileInput
-                          setImageUrl={setImageUrl}
-                          localImageUrl={localImageUrl}
-                          setLocalImageUrl={setLocalImageUrl}
-                          setError={setError}
-                          trigger={trigger}
-                          completedCrop={completedCrop}
-                          setCompletedCrop={setCompletedCrop}
-                          {...register("image")}
-                        />
-                      </FormControl>
-                    </Stack>
-                  </Box>
-                </DrawerBody>
+                          <InputGroup>
+                            <Input
+                              type="text"
+                              {...register("title")}
+                              placeholder="Titulo do boleto"
+                              isRequired
+                            />
+                          </InputGroup>
+                        </FormControl>
 
-                <DrawerFooter borderTopWidth="1px">
-                  <Button variant="outline" mr="auto" onClick={onClose}>
-                    Cancelar
-                  </Button>
-                  <Button type="submit" colorScheme="blue">
-                    Cadastrar
-                  </Button>
-                </DrawerFooter>
-              </DrawerContent>
-            </Box>
-          </Drawer>
-        </Flex>
-
-        {isLoading ? (
-          <Flex justify="center">
-            <Spinner />
-          </Flex>
-        ) : error ? (
-          <Flex>
-            <Text>Sem boletos no momento</Text>
-          </Flex>
-        ) : (
-          <>
-            <Table overflowX="auto">
-              <Thead borderBottom="4px solid #DFE0EB">
-                <Tr>
-                  <Th px={["4", "4", "6"]} color="gray.300" width="8"></Th>
-                  <Th>Título</Th>
-                  <Th>Cliente</Th>
-                  <Th>Descrição</Th>
-                  <Th>Valor</Th>
-                  <Th>Status</Th>
-                  {isWideVersion && <Th>Data de vencimento</Th>}
-                </Tr>
-              </Thead>
-              <Tbody>
-                {data?.documents.map((document) => {
-                  return (
-                    <>
-                      <Tr
-                        key={document.id}
-                        verticalAlign="middle"
-                        borderBottom="2px solid #DFE0EB"
-                      >
-                        <Td>
-                          <Avatar
-                            name={document.fileUrl}
-                            src={document.fileUrl}
-                            size="lg"
-                            onClick={() => setOpenedModal(!openedModal)}
-                          />
-                        </Td>
-                        <Td>
-                          <Text fontWeight="bold">{document.title}</Text>
-                        </Td>
-                        <Td>
-                          <Text fontSize="sm">{document.owner}</Text>
-                        </Td>
-                        <Td>
-                          <Text fontSize="sm">{document.description}</Text>
-                        </Td>
-                        <Td>
-                          <Text fontWeight="bold">{document.value}</Text>
-                        </Td>
-                        <Td>
-                          <Checkbox
-                            size="lg"
-                            colorScheme="green"
-                            defaultChecked={document.paid}
-                            mx="auto"
-                            id={document.id}
-                            onChange={(e) => handleStatusChange(e)}
+                        <FormControl id="description">
+                          <FormLabel
+                            fontSize="0.75rem"
+                            fontWeight="bold"
+                            color="#9FA2B4"
                           >
-                            {document.paid ? "Pago" : "Não pago"}
-                          </Checkbox>
-                        </Td>
-                        {isWideVersion && <Td>{document.dueDate}</Td>}
-                      </Tr>
-                      <ViewImage
-                        imgUrl={document.fileUrl}
-                        openedModal={openedModal}
-                        setOpenedModal={setOpenedModal}
-                      />
-                    </>
-                  );
-                })}
-              </Tbody>
-            </Table>
-          </>
-        )}
-      </Box>
-    </Flex>
+                            Descrição
+                          </FormLabel>
+
+                          <InputGroup>
+                            <Input
+                              type="text"
+                              {...register("description")}
+                              placeholder="Mais informações"
+                              isRequired
+                            />
+                          </InputGroup>
+                        </FormControl>
+
+                        <FormControl id="clientId">
+                          <FormLabel
+                            fontSize="0.75rem"
+                            fontWeight="bold"
+                            color="#9FA2B4"
+                          >
+                            CLIENTE
+                          </FormLabel>
+
+                          <InputGroup marginBottom="24px">
+                            <Select
+                              isRequired
+                              {...register("clientId")}
+                              placeholder="Selecione o cliente"
+                            >
+                              {clientData?.clients.map((client) => (
+                                <option key={client.id} value={client.id}>
+                                  {client.name}
+                                </option>
+                              ))}
+                            </Select>
+                          </InputGroup>
+
+                          <FileInput
+                            setImageUrl={setImageUrl}
+                            localImageUrl={localImageUrl}
+                            setLocalImageUrl={setLocalImageUrl}
+                            setError={setError}
+                            trigger={trigger}
+                            completedCrop={completedCrop}
+                            setCompletedCrop={setCompletedCrop}
+                            {...register("image")}
+                          />
+                        </FormControl>
+                      </Stack>
+                    </Box>
+                  </DrawerBody>
+
+                  <DrawerFooter borderTopWidth="1px">
+                    <Button variant="outline" mr="auto" onClick={onClose}>
+                      Cancelar
+                    </Button>
+                    <Button type="submit" colorScheme="blue">
+                      Cadastrar
+                    </Button>
+                  </DrawerFooter>
+                </DrawerContent>
+              </Box>
+            </Drawer>
+          </Flex>
+
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex>
+              <Text>Sem boletos no momento</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table overflowX="auto">
+                <Thead borderBottom="4px solid #DFE0EB">
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8"></Th>
+                    <Th>Título</Th>
+                    <Th>Cliente</Th>
+                    <Th>Descrição</Th>
+                    <Th>Valor</Th>
+                    <Th>Status</Th>
+                    {isWideVersion && <Th>Data de vencimento</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.documents.map((document) => {
+                    return (
+                      <>
+                        <Tr
+                          key={document.id}
+                          verticalAlign="middle"
+                          borderBottom="2px solid #DFE0EB"
+                        >
+                          <Td>
+                            <Avatar
+                              name={document.fileUrl}
+                              src={document.fileUrl}
+                              size="lg"
+                              onClick={() => {
+                                setOpenedModal(!openedModal);
+                                setImageUrl(document.fileUrl);
+                              }}
+                            />
+                          </Td>
+                          <Td>
+                            <Text fontWeight="bold">{document.title}</Text>
+                          </Td>
+                          <Td>
+                            <Text fontSize="sm">{document.owner}</Text>
+                          </Td>
+                          <Td>
+                            <Text fontSize="sm">{document.description}</Text>
+                          </Td>
+                          <Td>
+                            <Text fontWeight="bold">{document.value}</Text>
+                          </Td>
+                          <Td>
+                            <Checkbox
+                              size="lg"
+                              colorScheme="green"
+                              defaultChecked={document.paid}
+                              mx="auto"
+                              id={document.id}
+                              onChange={(e) => handleStatusChange(e)}
+                            >
+                              {document.paid ? "Pago" : "Não pago"}
+                            </Checkbox>
+                          </Td>
+                          {isWideVersion && <Td>{document.dueDate}</Td>}
+                        </Tr>
+                      </>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </>
+          )}
+        </Box>
+      </Flex>
+      <ViewImage
+        imgUrl={imageUrl}
+        openedModal={openedModal}
+        setOpenedModal={setOpenedModal}
+      />
+    </>
   );
 }
