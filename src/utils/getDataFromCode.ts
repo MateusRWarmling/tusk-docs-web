c1158970f3e9ec184d3525e4beb5e90d4b6c84a1
Gustavo Warmling <gustavowarmling@gmail.com>
2021-11-10 01:20:28 -0300

    Add: Get data from barcode function

--- src/utils/getDataFromCode.ts
+++ src/utils/getDataFromCode.ts
@@ -1,21 +1,17 @@
 import { add } from "date-fns";
 
 export function getDataFromCode(code: String) {
-  // console.log(`codigo: ${code}`);
-
   const baseYear = new Date(1997, 9, 7);
 
-  // console.log(`Ano base: ${baseYear}`);
-
-  const daysPassed = Number([...code.slice(5, 9)].join(""));
-  const value = Number([...code.slice(9, 19)].join(""));
-
-  console.log(`Dias passados: ${daysPassed}`);
-  console.log(`Valor: ${value}`);
+  const daysPassed = Number(code.slice(5, 9));
+  const value = Number(`${code.slice(9, 17)}.${code.slice(17, 19)}`);
 
   const dueDate = add(baseYear, {
     days: daysPassed,
   });
 
-  console.log(`Ano final: ${dueDate}`);
+  return {
+    dueDate,
+    value,
+  };
 }
